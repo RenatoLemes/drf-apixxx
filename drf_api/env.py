18c7c00e4932f4b9b3ffff2621ece4ad462f97b8
RenatoLemes <renato.lemmes@hotmail.com>
2023-07-11 08:42:30 +0000

    This is my first commit

---
+++ drf_api/env.py
@@ -0,0 +1,5 @@
+import os
+os.environ['CLOUDINARY_URL'] = 'cloudinary://418193753265483:nkgHom4eTX9OomydNiOWYZj2iNA@dyi0up7ld',
+os.environ['CLOUD_NAME'] = 'dyi0up7ld',
+os.environ['API_KEY'] = '418193753265483',
+os.environ['API_SECRET'] = 'nkgHom4eTX9OomydNiOWYZj2iNA',
