a380b7948faf5abffb8b400861c2cf2cda99f4f7
RenatoLemes <renato.lemmes@hotmail.com>
2023-07-13 18:18:04 +0000

    commit to be coninued later

--- drf_api/env.py
+++ drf_api/env.py
@@ -3,4 +3,5 @@ os.environ['CLOUDINARY_URL'] = 'cloudinary://418193753265483:nkgHom4eTX9OomydNiO
 os.environ['CLOUD_NAME'] = 'dyi0up7ld',
 os.environ['API_KEY'] = '418193753265483',
 os.environ['API_SECRET'] = 'nkgHom4eTX9OomydNiOWYZj2iNA',
-os.environ['DEV'] = '1'
+# os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = "<postgres://eagchbpl:GyjcYPZ6bGoZ7JYqeEB8SgqWdG5KN63c@trumpet.db.elephantsql.com/eagchbpl>",
