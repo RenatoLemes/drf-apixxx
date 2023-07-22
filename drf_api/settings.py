219d24e6622ac09aee38d7d4b42d43bdbe6df61b
RenatoLemes <renato.lemmes@hotmail.com>
2023-07-22 11:30:40 +0000

    test

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,7 +69,7 @@ ALLOWED_HOSTS = ['8000-renatolemes-drfapi-vg760ce6tey.ws-eu101.gitpod.io',
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+        os.environ.get('https://moments-renato-f4e49e9fdd1f.herokuapp.com') #CLIENT_ORIGIN
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
